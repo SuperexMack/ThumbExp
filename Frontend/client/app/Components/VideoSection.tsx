96304097b0ed0bdb9ca13e5d50e9b62a0692b179
SuperexMack <unilexerox@gmail.com>
2026-05-21 12:00:40 +0530

    Made a new thumbnail and added to the landing page

--- Frontend/client/app/Components/VideoSection.tsx
+++ Frontend/client/app/Components/VideoSection.tsx
@@ -6,14 +6,14 @@ export function HeroVideoDialogDemoTopInBottomOut() {
       <HeroVideoDialog
         className="block dark:hidden"
         animationStyle="top-in-bottom-out"
-        videoSrc="https://www.youtube.com/embed/qh3NGpYRG3I?si=4rb-zSdDkVK9qxxb"
+        videoSrc="https://www.youtube.com/embed/9cWuEK1NkK0?si=zLg5mXMgJOj2mz-z"
         thumbnailSrc="/mainvideo.png"
         thumbnailAlt="Hero Video"
       />
       <HeroVideoDialog
         className="hidden dark:block"
         animationStyle="top-in-bottom-out"
-        videoSrc="https://www.youtube.com/embed/qh3NGpYRG3I?si=4rb-zSdDkVK9qxxb"
+        videoSrc="https://www.youtube.com/embed/9cWuEK1NkK0?si=zLg5mXMgJOj2mz-z"
         thumbnailSrc="/mainvideo.png"
         thumbnailAlt="Hero Video"
       />
--- Frontend/client/app/Components/bottomHeader.tsx
+++ Frontend/client/app/Components/bottomHeader.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 export function BottomContainer() {
   return (
     <>
-      <div className="w-full bg-[#0A020A] p-5 mt-[6rem] md:mt-[10rem] border-t-2 border-b-2 border-t-slate-300 border-b-slate-300 flex flex-col items-center justify-center">
+      <div className="w-full bg-black p-5 mt-[6rem] md:mt-[10rem] border-t-2 border-b-2 border-t-slate-300 border-b-slate-300 flex flex-col items-center justify-center">
         <div className="md:w-[40%] w-full flex flex-col items-center mt-[30px]">
           <h1 className="md:text-[40px] text-[25px] maintext text-center">
             Make your first thumbnail in seconds.
